33844f8e93f34df5da839b9422d042acc6950b43
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-03-29 15:46:17 +0500

    Bismillah 🥇

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int biggest;
+        if (first >= second) {
+            biggest = first >= third ? first : third;
+        } else {
+            biggest = second >= third ? second : third;
+        }
+        System.out.println(biggest);
     }
 }
